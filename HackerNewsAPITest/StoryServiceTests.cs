4d79490eb9fce040e339a6d0b4635d1ac170c38a
Ashish Patil <ashishbpatil1@gmail.com>
2024-06-03 12:51:41 +0530

    Implemented changes for IMemoryCache moq

--- HackerNewsAPITest/StoryServiceTests.cs
+++ HackerNewsAPITest/StoryServiceTests.cs
@@ -1,11 +1,18 @@
 using System.Net;
+using System.Net.Http;
 using System.Text.Json;
 using HackerNewsAPI.Models;
 using HackerNewsAPI.Services;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.Protected;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HackerNewsAPITest
 {
@@ -14,28 +21,59 @@ namespace HackerNewsAPITest
     {
         private Mock<ILogger<StoryService>> _loggerMock;
         private Mock<IConfiguration> _configMock;
+        private Mock<IHttpClientFactory> _httpClientFactoryMock;
         private Mock<HttpMessageHandler> _httpMessageHandlerMock;
-        private HttpClient _httpClient;
+        private Mock<IMemoryCache> _cacheMock;
         private StoryService _storyService;
+        private HttpClient _httpClient;
 
         [TestInitialize]
         public void Setup()
         {
             _loggerMock = new Mock<ILogger<StoryService>>();
             _configMock = new Mock<IConfiguration>();
-            _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
-            _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            _cacheMock = new Mock<IMemoryCache>();
 
             _configMock.Setup(config => config["API:TopStories"]).Returns("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
             _configMock.Setup(config => config["API:Story"]).Returns("https://hacker-news.firebaseio.com/v0/item/#id#.json?print=pretty");
 
-            _storyService = new StoryService(_loggerMock.Object, _configMock.Object, _httpClient);
+            _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            _httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(_httpClient);
+
+            _storyService = new StoryService(
+                _loggerMock.Object,
+                _configMock.Object,
+                _httpClientFactoryMock.Object,
+                _cacheMock.Object
+            );
+        }
+
+        [TestMethod]
+        public async Task GetTopStoriesAsync_ShouldReturnTopStories_FromCache()
+        {
+            // Arrange
+            var cachedStories = new List<int> { 1, 2, 3 };
+            object cacheEntry = cachedStories;
+            _cacheMock.Setup(c => c.TryGetValue("TopStories", out cacheEntry)).Returns(true);
+
+            // Act
+            var result = await _storyService.GetTopStoriesAsync(3);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count());
+            CollectionAssert.AreEqual(cachedStories, result.ToList());
         }
 
         [TestMethod]
         public async Task GetTopStoriesAsync_ShouldReturnTopStories()
         {
             // Arrange
+            object cacheEntry = null;
+            _cacheMock.Setup(c => c.TryGetValue("TopStories", out cacheEntry)).Returns(false);
+            _cacheMock.Setup(c => c.CreateEntry(It.IsAny<object>())).Returns(Mock.Of<ICacheEntry>);
             var topStoriesJson = JsonSerializer.Serialize(Enumerable.Range(1, 300).ToList());
             var httpResponseMessage = new HttpResponseMessage
             {
@@ -45,7 +83,7 @@ namespace HackerNewsAPITest
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>(
-                    "GetAsync",
+                    "SendAsync",
                     ItExpr.IsAny<HttpRequestMessage>(),
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
@@ -55,11 +93,10 @@ namespace HackerNewsAPITest
 
             // Assert
             Assert.AreEqual(200, result.Count());
-            Assert.IsTrue(result.SequenceEqual(Enumerable.Range(1, 200)));
+            CollectionAssert.AreEqual(Enumerable.Range(1, 200).ToList(), result.ToList());
         }
 
         [TestMethod]
-        [ExpectedException(typeof(HttpRequestException))]
         public async Task GetTopStoriesAsync_ShouldThrowHttpRequestException_OnHttpError()
         {
             // Arrange
@@ -70,17 +107,16 @@ namespace HackerNewsAPITest
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>(
-                    "GetAsync",
+                    "SendAsync",
                     ItExpr.IsAny<HttpRequestMessage>(),
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
 
-            // Act
-            await _storyService.GetTopStoriesAsync(200);
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<HttpRequestException>(() => _storyService.GetTopStoriesAsync(200));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(JsonException))]
         public async Task GetTopStoriesAsync_ShouldThrowJsonException_OnInvalidJson()
         {
             // Arrange
@@ -93,13 +129,13 @@ namespace HackerNewsAPITest
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>(
-                    "GetAsync",
+                    "SendAsync",
                     ItExpr.IsAny<HttpRequestMessage>(),
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
 
-            // Act
-            await _storyService.GetTopStoriesAsync(200);
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<JsonException>(() => _storyService.GetTopStoriesAsync(200));
         }
 
         [TestMethod]
@@ -116,21 +152,40 @@ namespace HackerNewsAPITest
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>(
-                    "GetAsync",
+                    "SendAsync",
                     ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.ToString().Contains("https://hacker-news.firebaseio.com/v0/item/")),
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
+
+            var cachedStories = new Dictionary<string, StoryViewModel>
+            {
+                { "Story_1", new StoryViewModel { id = 1 } },
+                { "Story_2", new StoryViewModel { id = 2 } },
+                { "Story_3", new StoryViewModel { id = 3 } }
+            };
+            object cacheEntry = null;
+            _cacheMock.Setup(c => c.TryGetValue(It.IsAny<object>(), out cacheEntry))
+                .Returns((object key, out object value) =>
+                {
+                    value = cachedStories.GetValueOrDefault((string)key);
+                    return value != null;
+                });
 
             // Act
             var result = await _storyService.GetStoryAsync(ids);
 
             // Assert
             Assert.AreEqual(3, result.Count);
-            Assert.IsTrue(result.All(story => story.id == 1));
+            int i = 0;
+            foreach (var story in result)
+            {
+                Assert.AreEqual(ids[i], story.id);
+                i++;
+            }
         }
 
+
         [TestMethod]
-        [ExpectedException(typeof(HttpRequestException))]
         public async Task GetStoryByIdAsync_ShouldThrowHttpRequestException_OnHttpError()
         {
             // Arrange
@@ -146,12 +201,11 @@ namespace HackerNewsAPITest
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
 
-            // Act
-            await _storyService.GetStoryAsync(new List<int> { 1 });
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<HttpRequestException>(() => _storyService.GetStoryAsync(new List<int> { 1 }));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(JsonException))]
         public async Task GetStoryByIdAsync_ShouldThrowJsonException_OnInvalidJson()
         {
             // Arrange
@@ -164,13 +218,13 @@ namespace HackerNewsAPITest
 
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>(
-                    "GetAsync",
+                    "SendAsync",
                     ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.ToString().Contains("https://hacker-news.firebaseio.com/v0/item/")),
                     ItExpr.IsAny<CancellationToken>())
                 .ReturnsAsync(httpResponseMessage);
 
-            // Act
-            await _storyService.GetStoryAsync(new List<int> { 1 });
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<JsonException>(() => _storyService.GetStoryAsync(new List<int> { 1 }));
         }
     }
 }
